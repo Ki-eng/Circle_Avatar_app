7b2424c1f5916360caffb81b56df899798d5a750
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-09 21:50:18 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/computer.jpg'),
+                radius: 150,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
